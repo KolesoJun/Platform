322925915924e3737695d2f53cabbedd6be13dee
KolesoJun <danil187crr@yandex.ru>
2024-10-31 20:56:54 +0300

    1

--- Assets/Homework/Scripts/Coin.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Coin : MonoBehaviour
-{
-    [field: SerializeField]
-    public int Value { get; private set; }
-}
--- Assets/Homework/Scripts/MedicineKit.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class MedicineKit : MonoBehaviour
-{
-    [field: SerializeField]
-    public int CountHealth { get; private set; }
-}
